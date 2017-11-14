0d1b85945b5c8b0989638e4c698cd905d7e5a8c7
Harri Muhammad <harrimuhammad@gmail.com>
2017-11-14 21:34:15 +0700

    add view create

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -7,4 +7,13 @@ use Illuminate\Http\Request;
 class CustomerController extends Controller
 {
     //
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('customer.create');
+    }
 }
